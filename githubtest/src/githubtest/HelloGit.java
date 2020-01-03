0fc77b54de6d833bb904e1505f54702691318f98
Administrator <Administrator@QH-20190826ZHQZ>
2020-01-03 12:30:26 +0800

    777

--- githubtest/src/githubtest/HelloGit.java
+++ githubtest/src/githubtest/HelloGit.java
@@ -2,9 +2,13 @@ package githubtest;
 
 public class HelloGit {
 	public static void main(String[] args) {
+		
 		int b = 2;
 		int a = 1;
+		int mas = 3;
 		System.out.println("github");
+		
+	
 	}
 
 }
