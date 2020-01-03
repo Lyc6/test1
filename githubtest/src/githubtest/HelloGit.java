751aad49389d3adf01601b9f6356af7fcbb49851
Administrator <Administrator@QH-20190826ZHQZ>
2020-01-03 12:07:39 +0800

    01

---
+++ githubtest/src/githubtest/HelloGit.java
@@ -0,0 +1,8 @@
+package githubtest;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("github");
+	}
+
+}
--- githubtest/src/githubtest/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package githubtest;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		String s ="abc";
-		System.out.println("你好！");
-	}
-
-}
